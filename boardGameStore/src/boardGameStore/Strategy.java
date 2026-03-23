445a0a94be2bbf33aab01802151578193de4c0ad
Giovanni-Carrizzo <gcarrizzo@gmail.com>
2026-03-23 17:07:05 +0000

    Added Strategy class which inherits from the abstract Product class and is supposed to represent a strategy board game

---
+++ boardGameStore/src/boardGameStore/Strategy.java
@@ -0,0 +1,16 @@
+package boardGameStore;
+
+public class Strategy extends Product {
+
+	public Strategy(int prodId, ProductCategory prodCat, String prodName, double cost, int quantity, double prodPrice) {
+		super(prodId, prodCat, prodName, cost, quantity, prodPrice);
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
